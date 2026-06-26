6bb021f4678b10095613c7c6d88e907a239932fe
imam-programmer <imamprogrammer09@gmail.com>
2026-06-26 10:14:53 +0600

    some linkup issue solve of homepage

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div className='flex items-center justify-center gap-3'>
-        <Link className='text-[6vw] border-2 border-white rounded-full px-8 uppercase text-white'>Projects</Link>
-        <Link className='text-[6vw] border-2 border-white rounded-full px-8 uppercase text-white'>Agence</Link>
+    <div className='flex items-center justify-center gap-3 hover:border-green-400 hover:text-green-400'>
+        <Link className='text-[6vw] border-2 border-white rounded-full px-8 uppercase text-white hover:border-[#d3fd50] hover:text-[#d3fd50]' to="/projects">Projects</Link>
+        <Link className='text-[6vw] border-2 border-white rounded-full px-8 uppercase text-white hover:border-[#d3fd50] hover:text-[#d3fd50]' to="/agence">Agence</Link>
     </div>
   )
 }
