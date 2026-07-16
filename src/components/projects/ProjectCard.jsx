93ee43dcb6d7d5b636aadc5b20620b99d81c4ac5
imam-programmer <imamprogrammer09@gmail.com>
2026-07-16 20:20:14 +0600

    project part animation responsive done

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
 const ProjectCard = (props) => {
-    console.log(props)
+
   return (
        
        <>
-          <div className='w-1/2 group relative hover:rounded-4xl duration-300 overflow-hidden  md:h-full bg-green-900'>
+          <div className='md:w-1/2 w-full h-[45vh]  md:h-full group relative hover:rounded-4xl duration-300 overflow-hidden  '>
           <img className='h-full w-full object-cover' src={props.image1} alt="" />
           <div className='opacity-0 group-hover:opacity-100 duration-500 absolute top-0 left-0 flex items-center justify-center bg-black/50 h-full w-full'>
             <h2 className='uppercase text-5xl text-amber-50 py-2 px-5 cursor-pointer border-2 rounded-full'>vior le projet</h2>
           </div>
           </div>
-           <div className='w-1/2 group relative hover:rounded-4xl duration-300 overflow-hidden h-full bg-green-900'>
+           <div className='md:w-1/2 w-full h-[45vh]   md:h-full  group relative hover:rounded-4xl duration-300 overflow-hidden '>
           <img className='h-full w-full object-cover' src={props.image2} alt="" />
           <div className='opacity-0 group-hover:opacity-100 duration-500 absolute top-0 left-0 flex items-center justify-center bg-black/50 h-full w-full'>
             <h2 className='uppercase text-5xl text-amber-50 py-2 px-5 cursor-pointer border-2 rounded-full'>vior le projet</h2>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,59 +6,66 @@ import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
 
 const Projects = () => {
-  const projects=[
+  const projects = [
     {
-      image1:"./image/navImage1.jpg",
-      image2:"./image/navimage2.jpg"
+      image1: "./image/navImage1.jpg",
+      image2: "./image/navimage2.jpg"
     },
     {
-      image1:"./image/navimage3.jpg",
-      image2:"./image/navimage4.jpg"
+      image1: "./image/navimage3.jpg",
+      image2: "./image/navimage4.jpg"
     },
-      {
-        image2:"./image/navImage1.jpg",
-        image1:"./image/navimage4.jpg"
+    {
+      image2: "./image/navImage1.jpg",
+      image1: "./image/navimage4.jpg"
     }
   ]
 
 
   gsap.registerPlugin(ScrollTrigger)
 
-useGSAP(function(){
-  gsap.from(".hero",{
-    height:0,
-    stagger:{
-      amount:0.5
-    },
-    scrollTrigger:{
-      trigger:".lol",
-    start:"top 100%",
-    end:"top -200%",
-    scrub:true
-    }
-  })
-})
+  useGSAP(function () {
+    gsap.set(".hero", { transformOrigin: "top" });
+    gsap.from(".hero", {
+      height: 0,
+      stagger: {
+        amount: 0.5
+      },
+      scrollTrigger: {
+        trigger: ".lol",
+        start: "top 100%",
+        end: "top -200%",
+        scrub: 1
+      }
+    })
+    const handleLoad = () => ScrollTrigger.refresh();
+    window.addEventListener("load", handleLoad);
+
+    return () => window.removeEventListener("load", handleLoad);
+  }, [])
 
   return (
     <>
-    <Navbar className="text-black"/>
-    <div className='lg:p-4 p-2 mb-[100vh]'>
-      <div className=' pt-[45vh]'>
-        <h2 className='text-[10vw] uppercase'>Projects</h2>
+      <Navbar className="text-black" />
+      <div className='lg:p-4 p-2 bg-[#bebebe]'>
+        <div className=' pt-[55vh] xs:pt-[60vh] sm:pt-[60vh] md:pt-[45vh]'>
+          <h2 className='lg:text-[10vw] text-6xl uppercase'>Projects</h2>
+        </div>
+        <div className='  lol'>
+          {
+            projects.map((elem, idx) => (
+              <div key={idx} className='hero w-full  h-[90vh] sm:h-[60vh]  lg:h-[80vh] mb-4 flex flex-col md:flex-row lg:gap-10 gap-2'>
+                <ProjectCard image1={elem.image1} image2={elem.image2} />
+              </div>
+            ))
+          }
+
+
+        </div>
       </div>
-      <div className=' lg:-mt-10 lol'>
-        {
-          projects.map((elem,idx)=>(
- <div key={idx} className='hero w-full  lg:h-[600.5px] mb-4 flex lg:gap-10 gap-2'>
-            <ProjectCard image1={elem.image1} image2={elem.image2}/>
-            </div>
-          ))
-        }
- 
-    
+      <div className='h-screen flex justify-center items-center bg-[#196ae4bb]'>
+        <h2 className='lg:text-[10vw] text-6xl uppercase font-bold project-h2'><i>Thank You</i></h2>
       </div>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum pariatur libero molestias aliquid, temporibus provident maiores natus, deleniti earum quis consectetur voluptatum possimus quam quae modi sit ipsum similique voluptates? Aperiam voluptatem explicabo sed doloremque earum exercitationem aut facere quisquam autem, fugit non molestiae eos minus, eveniet numquam minima vel atque molestias ut ipsum qui vero reprehenderit. In quod voluptatem eaque nemo iusto id quia reprehenderit dolor fugiat quaerat libero distinctio harum architecto sed, quos velit ex cupiditate labore repudiandae! Quis, assumenda dolor et natus hic error iste, voluptatem quas numquam dignissimos recusandae beatae necessitatibus, deserunt praesentium molestias perspiciatis est!</p>
-    </div>
     </>
   )
 }
