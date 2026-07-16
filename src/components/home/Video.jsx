7d3668b806a02eb08e5d5c5a32ee8e36dc58e5d2
imam-programmer <imamprogrammer09@gmail.com>
2026-07-16 22:51:34 +0600

    video link issue secound time test

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video autoPlay muted loop className='h-full w-full object-cover' src="../../../public/homeVideo.mp4">
+        <video autoPlay muted loop className='h-full w-full object-cover' src="./homeVideo.mp4">
            {/* <source  type="video/mp4" /> */}
         </video>
     </div>
